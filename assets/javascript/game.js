7907d789f242cf71e13b34eac4590daff6bfd450
GreenEyes <flddnick@gmail.com>
2018-11-13 23:55:42 -0600

    completed css flare

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 
 var gameObjective = (Math.floor(Math.random() * (101 + 19)));
-
 var gemPoints = (Math.floor(Math.random() * (11 + 1)));
+var yourScore = 0;
+var yourWins = 0;
+var yourLosses = 0;
